0edede2c6cfc2542a88838a468b7a8eddbece0a8
asafperez02-cpu <asafperez02@gmail.com>
2026-04-29 22:24:02 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,21 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore"; // <-- הוספנו את ייבוא מסד הנתונים
 
+// ההגדרות החדשות של לני
 const firebaseConfig = {
-  apiKey: "AIzaSyDrjTKgdOoPxV18vbVxImfy7rBqnxQfcYY",
-  authDomain: "baby-monitor-57bb9.firebaseapp.com",
-  projectId: "baby-monitor-57bb9",
-  storageBucket: "baby-monitor-57bb9.firebasestorage.app",
-  messagingSenderId: "143290491509",
-  appId: "1:143290491509:web:352c1e25b01c7bde710e3c"
+  apiKey: "AIzaSyCDtgAl4ubWZUEDlAeRGumTnH4W5ZdS_EE",
+  authDomain: "baby-app-leni.firebaseapp.com",
+  projectId: "baby-app-leni",
+  storageBucket: "baby-app-leni.firebasestorage.app",
+  messagingSenderId: "970432827513",
+  appId: "1:970432827513:web:7202d05b60b1919d5344a8",
+  measurementId: "G-NFWZC3GNSX"
 };
 
+// אתחול האפליקציה
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// ייצוא החיבור למסד הנתונים כדי ש-App.jsx יוכל להשתמש בו
 export const db = getFirestore(app);
